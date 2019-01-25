d41a4cace5b57c9eff15f65f24c877b362ffb0ac
sufo6969 <43465892+sufo6969@users.noreply.github.com>
2019-01-25 13:25:25 +0100

    Add project files.

---
+++ Exempel/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Exempel.Startup))]
+namespace Exempel
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
